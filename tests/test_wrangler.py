2de76a958fc6cdcfc051522ba39f3562a9f42226
PhilStedman <philippestedman@gmail.com>
2021-04-13 10:46:18 -0700

    Rename csvwrangler to wrangler

--- tests/test_wrangler.py
+++ tests/test_wrangler.py
@@ -1,15 +1,15 @@
-from philcsv import csvwrangler
+from philcsv import wrangler
 from pathlib import Path
 import datetime
 
 
 def test_samplecsv():
     file = Path(__file__).parent / "resources/sample.csv"
-    orders = csvwrangler.wrangle(file)
+    orders = wrangler.wrangle(file)
 
     expected = [
-        csvwrangler.Order(1000, datetime.datetime(2018, 1, 1), "P-10001", "Arugola", 5250.50, "kg"),
-        csvwrangler.Order(
+        wrangler.Order(1000, datetime.datetime(2018, 1, 1), "P-10001", "Arugola", 5250.50, "kg"),
+        wrangler.Order(
             1001,
             datetime.datetime(2017, 12, 12),
             "P-10002",
@@ -34,11 +34,11 @@ def test_samplecsv_withcfg():
     file = Path(__file__).parent / "resources/sample.csv"
     cfg_file = Path(__file__).parent / "resources/config1.ini"
 
-    orders = csvwrangler.wrangle(file, cfg_file)
+    orders = wrangler.wrangle(file, cfg_file)
 
     expected = [
-        csvwrangler.Order(1000, datetime.datetime(2018, 1, 1), "P-10001", "Arugola", 5250, "lbs"),
-        csvwrangler.Order(
+        wrangler.Order(1000, datetime.datetime(2018, 1, 1), "P-10001", "Arugola", 5250, "lbs"),
+        wrangler.Order(
             1001,
             datetime.datetime(2017, 12, 12),
             "P-10002",
@@ -61,11 +61,11 @@ def test_samplecsv_withcfg():
 
 def test_errorcsv():
     file = Path(__file__).parent / "resources/erroneous.csv"
-    orders = csvwrangler.wrangle(file)
+    orders = wrangler.wrangle(file)
 
     expected = [
-        csvwrangler.Order(1000, datetime.datetime(2020, 6, 27), "P-9001", "Chocolate", 5.25, "kg"),
-        csvwrangler.Order(1001, datetime.datetime(2019, 12, 12), "P-10002", "Bagel", 15, "kg"),
+        wrangler.Order(1000, datetime.datetime(2020, 6, 27), "P-9001", "Chocolate", 5.25, "kg"),
+        wrangler.Order(1001, datetime.datetime(2019, 12, 12), "P-10002", "Bagel", 15, "kg"),
     ]
 
     i = 0
