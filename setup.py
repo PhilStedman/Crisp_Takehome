2dead089a28dec4dc0bf3fb46c9e02d16cf3ed1e
PhilStedman <philippestedman@gmail.com>
2021-04-12 17:05:52 -0700

    Remove pytest/pytest-runner from setup file

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from setuptools import find_packages, setup
 
+with open("README.md") as fp:
+   long_description = fp.read()
+
 setup(
     name='philcsv',
     python_requires='>=3.8.5',
     packages=find_packages(include=['philcsv']),
     version='0.1.0',
     description='Crisp Take Home Library',
+    long_description=long_description,
     author='Phil Stedman',
     license='GPL',
     install_requires=['pandas'],
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest'],
-    test_suite='tests',
 )
